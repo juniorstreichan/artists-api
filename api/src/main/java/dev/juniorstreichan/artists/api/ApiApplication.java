18c3a56b6ad652e5ab659ad3660cd92029670239
Junior Streichan <juniorstreichan.dev@gmail.com>
2019-11-05 23:37:25 -0400

    Pequenos ajustes

--- api/src/main/java/dev/juniorstreichan/artists/api/ApiApplication.java
+++ api/src/main/java/dev/juniorstreichan/artists/api/ApiApplication.java
@@ -2,8 +2,12 @@ package dev.juniorstreichan.artists.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan({"dev.juniorstreichan.artists.core.model"})
+@EnableJpaRepositories({"dev.juniorstreichan.artists.core.repository"})
 public class ApiApplication {
 
 	public static void main(String[] args) {
